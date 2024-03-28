f34790b0cf50837f76ce8a75adbf938a76300903
astrogroupsl-dev <nadeeshafernando2020@gmail.com>
2024-03-28 23:52:04 +0530

    profile update

---
+++ js/profile.js
@@ -0,0 +1,6 @@
+function nextStep(step) {
+    document.querySelector('.active').classList.remove('active');
+    document.getElementById('step' + step).classList.add('active');
+    document.querySelector('.active').classList.remove('active');
+    document.getElementById('step' + step).classList.add('active');
+    }
